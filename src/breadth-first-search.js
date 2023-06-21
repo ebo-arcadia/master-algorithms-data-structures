1913f2f4d9f99c15c0c18a36f00a7594be603115
Ebo Arcadia <yiboli.coding@gmail.com>
2023-06-21 19:33:57 -0400

    breadth first search

--- src/breadth-first-search.js
+++ src/breadth-first-search.js
@@ -72,6 +72,26 @@ class BinarySearchTree {
     }
     return false;
   }
+  // breadth first search
+  breadthFirstSearch() {
+    let result = [];
+    let queue = [];
+    let node = this.root;
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      result.push(node.value);
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    return result;
+  }
 }
 
 const tree = new BinarySearchTree();
@@ -81,4 +101,4 @@ tree.insert(11);
 tree.insert(6);
 tree.insert(9);
 tree.insert(13);
-console.log(tree);
+console.info(tree.breadthFirstSearch());
