ed2b16cebd73b5344e94b8e33d7619f11c391912
Ebo Arcadia <yiboli.coding@gmail.com>
2023-07-10 10:59:27 -0400

    extract Max initial solution

--- src/binary-heaps.js
+++ src/binary-heaps.js
@@ -24,12 +24,43 @@ class MaxBinaryHeap {
     }
   }
 
+  extractMax() {
+    const maxToExtract = this.values[0];
+    const childIndex = this.values.length - 1;
+    this.values[0] = this.values[childIndex];
+
+    let parentIndex = 0;
+    let parentValue = this.values[parentIndex];
+
+    while (parentValue <= leftChildIndex || parentValue <= rightChildValue) {
+      let leftChildIndex = Math.floor(2 * parentIndex + 1);
+      let rightChildIndex = Math.floor(2 * parentIndex + 2);
+      let leftChildValue = this.values[leftChildIndex];
+      let rightChildValue = this.values[rightChildIndex];
+
+      if (parentIndex <= leftChildIndex && parentIndex <= rightChildIndex)
+        break;
+
+      if (leftChildIndex > rightChildIndex) {
+        this.values[parentIndex] = this.values[leftChildIndex];
+        parentIndex = leftChildIndex;
+      }
+
+      if (leftChildIndex < rightChildIndex) {
+        this.values[parentIndex] = this.values[rightChildIndex];
+        parentIndex = rightChildIndex;
+      }
+    }
+
+    return maxToExtract;
+  }
+
   print() {
     console.info(this.values);
   }
 }
 
 const maxBinaryHeap = new MaxBinaryHeap([41, 39, 55, 18, 27, 12]);
-console.info(maxBinaryHeap);
 maxBinaryHeap.insert(100);
+// console.info(maxBinaryHeap);
 maxBinaryHeap.print();
