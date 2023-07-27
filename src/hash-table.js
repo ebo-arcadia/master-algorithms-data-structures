07acc2e3dca5b7a7faf9f7bbbf7bd13118af257c
Ebo Arcadia <yiboli.coding@gmail.com>
2023-07-27 11:08:12 -0400

    hash table class

--- src/hash-table.js
+++ src/hash-table.js
@@ -1,12 +1,16 @@
-function hash(key, arrayLen) {
-  let total = 0;
-  const random_prime = 31;
-  for (let c = 0; c < Math.min(key.length, 100); c++) {
-    let char = key[c];
-    let value = char.charCodeAt(0) - 96;
-    total = (total * random_prime + value) % arrayLen;
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
   }
-  return total;
-}
 
-console.info(hash('bio', 5));
+  _hash(key) {
+    let total = 0;
+    let random_prime = 31;
+    for (let c = 0; c < Math.min(key.length, 100); c++) {
+      let char = key[c];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * random_prime + value) % this.keyMap.length;
+    }
+    return total;
+  }
+}
