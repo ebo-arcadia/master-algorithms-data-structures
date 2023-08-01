daf0fab5fc006577ceade807eff133d5ada25f46
Ebo Arcadia <yiboli.coding@gmail.com>
2023-08-01 11:17:04 -0400

    hash values, keys method

--- src/hash-table.js
+++ src/hash-table.js
@@ -33,13 +33,51 @@ class HashTable {
     }
     return undefined;
   }
+
+  keys() {
+    let keys = [];
+    for (let c = 0; c < this.keyMap.length; c++) {
+      if (this.keyMap[c]) {
+        for (let i = 0; i < this.keyMap[c].length; i++) {
+          if (this.keyMap[c][i]) {
+            if (!keys.includes(this.keyMap[c][i][0])) {
+              keys.push(this.keyMap[c][i][0]);
+            }
+          }
+        }
+      }
+    }
+    return keys;
+  }
+
+  values() {
+    let values = [];
+    for (let c = 0; c < this.keyMap.length; c++) {
+      if (this.keyMap[c]) {
+        for (let i = 0; i < this.keyMap[c].length; i++) {
+          if (this.keyMap[c][i]) {
+            if (!values.includes(this.keyMap[c][i][1])) {
+              values.push(this.keyMap[c][i][1]);
+            }
+          }
+        }
+      }
+    }
+    return values;
+  }
 }
 
 // testing
 let hashTable = new HashTable(10);
 hashTable.set('I love', 'cats and dogs');
+hashTable.set('I love', 'computer programming');
 hashTable.set('Home inspection', 'cement the chimney');
-hashTable.set('get it done', 'programming is fun');
+hashTable.set('Home inspection', 'replace the ceiling');
+hashTable.set('daily routines', 'understand problems');
 hashTable.set('cloud computing', 'conference');
-// console.log(hashTable);
-console.log(hashTable.get('get it done'));
+hashTable.set('artificial intelligence', 'conference');
+hashTable.set('artificial intelligence', 'new career');
+console.log('hash table instance: ', hashTable);
+console.log('using get method: ', hashTable.get('Home inspection'));
+console.log('get all non dup values: ', hashTable.values());
+console.log('get all non dup keys: ', hashTable.keys());
