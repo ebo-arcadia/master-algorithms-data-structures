72d9e0f12079b8df2348a1128062899fcb512b1f
Ebo Arcadia <yiboli.coding@gmail.com>
2023-08-10 17:45:57 -0400

    fix add edge, remove edge

--- src/graph.js
+++ src/graph.js
@@ -9,11 +9,16 @@ class Graph {
 
   addEdge(vertex1, vertex2) {
     if (this.adjacencyList[vertex1]) {
-      this.adjacencyList[vertex1] = vertex2;
+      this.adjacencyList[vertex1].push(vertex2);
     }
     if (this.adjacencyList[vertex2]) {
-      this.adjacencyList[vertex2] = vertex1;
+      this.adjacencyList[vertex2].push(vertex1);
     }
+  }
+
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1);
   }
 }
 
@@ -22,4 +27,6 @@ graph.addVertex('UK');
 graph.addVertex('Mexico');
 graph.addVertex('Iceland');
 graph.addEdge('UK', 'Mexico');
+graph.addEdge('UK', 'Berlin');
+graph.removeEdge('UK', 'Mexico');
 console.log(graph);
