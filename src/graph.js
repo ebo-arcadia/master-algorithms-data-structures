d713c2e952af4a233f864cfc6d78d211a69e1969
Ebo Arcadia <yiboli.coding@gmail.com>
2023-08-28 13:46:17 -0400

    rename

--- src/graph.js
+++ src/graph.js
@@ -43,9 +43,9 @@ class Graph {
 
       visitedVertex[vertex] = true;
       result.push(vertex);
-      adjacencyList.forEach(vertex => {
-        if (!visitedVertex[vertex]) {
-          return helper(vertex);
+      adjacencyList[vertex].forEach(neighbor => {
+        if (!visitedVertex[neighbor]) {
+          return helper(neighbor);
         }
       });
     })(head);
@@ -54,14 +54,21 @@ class Graph {
   }
 }
 
-let graph = new Graph();
-graph.addVertex('UK');
-graph.addVertex('Mexico');
-graph.addVertex('Iceland');
-graph.addEdge('UK', 'Mexico');
-graph.addEdge('UK', 'Berlin');
-graph.addEdge('UK', 'Bali');
-graph.addEdge('Mexico', 'San Jose');
-graph.removeVertex('Mexico');
-console.log(graph);
-graph.depthFirstTraverse();
+let g = new Graph();
+
+g.addVertex('A');
+g.addVertex('B');
+g.addVertex('C');
+g.addVertex('D');
+g.addVertex('E');
+g.addVertex('F');
+
+g.addEdge('A', 'B');
+g.addEdge('A', 'C');
+g.addEdge('B', 'D');
+g.addEdge('C', 'E');
+g.addEdge('D', 'E');
+g.addEdge('D', 'F');
+g.addEdge('E', 'F');
+
+console.log(g.depthFirstTraverse('D'));
